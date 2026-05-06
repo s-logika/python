7ba91ee50dd29a6e6ced93164c41f43749bb32ea
logika <logikalogeswaran@gamil.com>
2026-05-06 14:55:02 +0530

    calulater

--- examples/calc.py
+++ examples/calc.py
@@ -1,9 +1,29 @@
-num1 = int(input("Enter Number1: "))
-num2 = int(input("Enter Number2: "))
-total = num1+num2
+num1 = int(input("Enter number1 :"))
+num2 = int(input("Enter number2 :"))
+option = int(input("selet option \n For Addition Enter 1 \n For Substraction Enter 2 \n For Multiplication Enter 3 \n For Divison Enter 4 \n Enter option: "))
 
-print(type(num1))
-print("Number1 = ", num1)
-print("Number2 = ", num2)
-print("Total = ", total)
+def add():
+    print("num1 + num2 = ", num1 + num2)
 
+def sub():
+    print("num1 - num2 = ", num1 - num2)
+
+def mul():
+    print("num1 * num2 = ", num1 * num2)
+
+def div():
+    print("num1 / num2 = ", num1 / num2)
+
+def calc(opt):
+    if opt == 1:
+        add()
+    elif opt == 2:
+        sub()
+    elif opt == 3:
+        mul()
+    elif opt == 4:
+        div()
+    else:
+        print("Invalid")
+
+calc(option)
